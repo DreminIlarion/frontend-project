82e76fbaa10559579cba673b7abd2a03d0a3746f
DreminIlarion <sonic.dremin@gmail.com>
2025-03-02 20:06:17 +0500

    попытка исправить вход

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,39 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { UserProvider } from './context/UserContext';
-import Login from './components/Login';
-import Profile from './components/Profile';
-
-import Register from './components/Register';
-import Chat from './components/Chat';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { UserProvider, useUser } from "./context/UserContext";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Register from "./components/Register";
+import Chat from "./components/Chat";
 import MailCallback from "./components/MailCallback";
 import YandexCallback from "./components/YandexCallback";
 import VKCallback from "./components/VKCallback";
-
 import PrivacyPolicy from "./components/PrivacyPolicy";
+import Home from "./components/Home";
+import Test from "./components/Cht";
 
-import Home from './components/Home';
+const ProtectedRoute = ({ element }) => {
+  const { user, loading } = useUser();
 
-import Test from './components/Cht';
+  if (loading) return <div>Загрузка...</div>; // Показываем загрузку
+  return user ? element : <Navigate to="/login" replace />;
+};
+
 const App = () => {
   return (
     <UserProvider>
       <Router>
         <Routes>
-          {/* Перенаправление с корневого пути на страницу профиля */}
           <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<ProtectedRoute element={<Profile />} />} />
           <Route path="/registration" element={<Register />} />
-
-          <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/chat" element={<Chat />} />
-
+          <Route path="/chat" element={<ProtectedRoute element={<Chat />} />} />
           <Route path="/vk/callback" element={<VKCallback />} />
           <Route path="/mail.ru/callback" element={<MailCallback />} />
           <Route path="/yandex/callback" element={<YandexCallback />} />
-          
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-
-          <Route path='/test' element={<Test />} ></Route>
+          <Route path="/test" element={<Test />} />
         </Routes>
       </Router>
     </UserProvider>
