bda8daedc31d7573ebe1727c78ee0ed71076de5a
DreminIlarion <sonic.dremin@gmail.com>
2025-03-03 16:29:02 +0500

    токены не работают

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,44 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import { UserProvider } from './context/UserContext';
 import Login from './components/Login';
 import Profile from './components/Profile';
-
 import Register from './components/Register';
 import Chat from './components/Chat';
-
 import MailCallback from "./components/MailCallback";
 import YandexCallback from "./components/YandexCallback";
 import VKCallback from "./components/VKCallback";
-
 import PrivacyPolicy from "./components/PrivacyPolicy";
-
 import Home from './components/Home';
-
 import Test from './components/Cht';
+
+const AppWrapper = () => {
+  const navigate = useNavigate(); // Создаем navigate внутри этого компонента
+
+  return (
+    <UserProvider navigate={navigate}> {/* Передаем navigate в контекст */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/registration" element={<Register />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/vk/callback" element={<VKCallback />} />
+        <Route path="/mail.ru/callback" element={<MailCallback />} />
+        <Route path="/yandex/callback" element={<YandexCallback />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path='/test' element={<Test />} />
+      </Routes>
+    </UserProvider>
+  );
+};
+
 const App = () => {
   return (
-    <UserProvider>
-      <Router>
-        <Routes>
-          {/* Перенаправление с корневого пути на страницу профиля */}
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/registration" element={<Register />} />
-
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/chat" element={<Chat />} />
-
-          <Route path="/vk/callback" element={<VKCallback />} />
-          <Route path="/mail.ru/callback" element={<MailCallback />} />
-          <Route path="/yandex/callback" element={<YandexCallback />} />
-          
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-
-          <Route path='/test' element={<Test />} ></Route>
-        </Routes>
-      </Router>
-    </UserProvider>
+    <Router>
+      <AppWrapper />
+    </Router>
   );
 };
 
